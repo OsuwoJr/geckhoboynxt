468bb68c58b1f3d1008bd4b3e2e64b89eb866ae4
OsuwoJr <osuwoodongo@gmail.com>
2025-05-13 22:57:45 +0300

    error fix

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -43,7 +43,6 @@ export const getContract = (provider: ethers.providers.Web3Provider) => {
 // Mint NFT function
 export const mintNFT = async (
   provider: ethers.providers.Web3Provider,
-  tokenId: number,
   tokenURI: string
 ) => {
   try {
@@ -112,8 +111,7 @@ export const getNFTBalance = async (provider: ethers.providers.Web3Provider, add
 
 // Get NFT collection info
 export const getNFTInfo = async (
-  provider: ethers.providers.Web3Provider,
-  tokenId: number
+  provider: ethers.providers.Web3Provider
 ) => {
   try {
     if (!provider) {
